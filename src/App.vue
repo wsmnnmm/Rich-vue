4d396c07dc124280e4bda9d0e6072afe7f93d778
wsmnnmm <1832004940@qq.com>
2022-01-13 21:39:22 +0800

    添加导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div id="app">
+  <div>
     <router-view/>
+    <hr>
+    <div>
+      <router-link to="/Money">记账</router-link>
+      <router-link to="/Labels">标签</router-link>
+      <router-link to="/Statistics">统计</router-link>
+    </div>
   </div>
 </template>
 
