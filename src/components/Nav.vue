10a44abc6ec0932da9b3e586543a2d4fb9c8f75a
wsmnnmm <1832004940@qq.com>
2022-01-14 17:51:21 +0800

    解决了 SVG 自带属性的问题

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/Money" class="item">
+    <router-link to="/Money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/Labels" class="item">
+    <router-link to="/Labels" class="item" active-class="selected">
       <Icon name="labels"/>
       标签
     </router-link>
-    <router-link to="/Statistics" class="item">
+    <router-link to="/Statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -26,6 +26,7 @@ nav {
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   flex-direction: row;
+
   > .item {
     padding: 2px 0;
     width: 33.333%;
@@ -33,10 +34,14 @@ nav {
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    > .icon{
+
+    .icon {
       width: 32px;
       height: 32px;
     }
+  }
+  > .item.selected {
+    color: green;
   }
 }
 </style>
