89589c591053e3886aa7938143e7f4247a069123
wsmnnmm <1832004940@qq.com>
2022-01-20 21:04:34 +0800

    添加 createdAt 字段

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,6 @@
 <template>
   <Layout class-prefix="layout">
-    {{ record }}
-    <NumberPad @update:value="onUpdateAmount"/>
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
@@ -11,28 +10,29 @@
 <script lang="ts">
 import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Tags from '@/components/Money/Tags.vue';
-import Notes from '@/components/Money/Notes.vue';
 import Types from '@/components/Money/Types.vue';
-import {Component} from 'vue-property-decorator';
+import Notes from '@/components/Money/Notes.vue';
+import Tags from '@/components/Money/Tags.vue';
+import {Component, Watch} from 'vue-property-decorator';
+
+const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
 
 type Record = {
   tags: string[]
   notes: string
   type: string
-  amount: number
+  amount: number // 数据类型 object | string
+  createdAt?: Date  // 类 / 构造函数
 }
+
 @Component({
-      components: {Types, Notes, Tags, NumberPad}
-    }
-)
+  components: {Tags, Notes, Types, NumberPad}
+})
 export default class Money extends Vue {
-  tags = ['衣', '食', '住', '行'];
+  tags = ['衣', '食', '住', '行', '彩票'];
+  recordList: Record[] = recordList;
   record: Record = {
-    tags: [],
-    notes: '',
-    type: '-',
-    amount: 0
+    tags: [], notes: '', type: '-', amount: 0
   };
 
   onUpdateTags(value: string[]) {
@@ -43,8 +43,15 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
 
-  onUpdateAmount(value: string) {
-    this.record.amount = parseFloat(value);
+  saveRecord() {
+    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    record2.createdAt = new Date();
+    this.recordList.push(record2);
+  }
+
+  @Watch('recordList')
+  onRecordListChange() {
+    window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
   }
 }
 </script>
