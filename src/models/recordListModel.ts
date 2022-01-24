0aa465cc6064d7bd6a1e2aa9100d4928d7144c2a
wsmnnmm <1832004940@qq.com>
2022-01-24 16:31:02 +0800

    重新封装recordListModel-2

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -8,8 +8,8 @@ const recordListModel = {
     this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
     return this.data;
   },
-  save(data: RecordItem[]) {
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+  save() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   }
 };
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -53,7 +53,7 @@ export default class Money extends Vue {
 
   @Watch('recordList')
   onRecordListChange() {
-    recordListModel.save(this.recordList);
+    recordListModel.save();
   }
 }
 </script>
