8245cec4dae6b8be33b977db7b79309f3a74aec5
wsmnnmm <1832004940@qq.com>
2022-01-24 22:10:18 +0800

    模块化的 store 删除了 models

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -3,7 +3,7 @@ type RecordItem = {
   notes: string,
   type: string,
   amount: number, //数据类型 object | string
-  createAt?: Date //类型 / 构造函数
+  createdAt?: Date //类型 / 构造函数
 }
 
 type Tag = {
@@ -20,5 +20,5 @@ type TagListModel = {
 }
 
 interface Window {
- 
+
 }
