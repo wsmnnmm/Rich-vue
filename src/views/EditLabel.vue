e5aeacec1a587e7a48d9e806d63be920c5b09ae1
wsmnnmm <1832004940@qq.com>
2022-01-25 19:32:38 +0800

    EditLabel.vue 使用 Vuex

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -26,10 +26,13 @@ import Button from '@/components/Button.vue';
   components: {Button, FormItem}
 })
 export default class EditLabel extends Vue {
-  tag?: Tag = undefined;
-  //TODO
-  //this.tag = //store.findTag(this)
+  get tag() {
+    return this.$store.state.currentTag;
+  }
+
   created() {
+    const id = this.$route.params.id;
+    this.$store.commit('setCurrentTag', id);
     if (!this.tag) {
       this.$router.replace('/404');
     }
