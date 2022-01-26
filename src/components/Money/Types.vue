a9b9135cd1b6c129cebb082d7fdc70a6b2339792
wsmnnmm <1832004940@qq.com>
2022-01-26 23:22:41 +0800

    抽离Tabs.vue 使用在Statistics.vue里面

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="value === '-' && 'selected'" @click="selectType('-')">支出</li>
-      <li :class="value === '+' && 'selected'" @click="selectType('+')">收入</li>
+      <li :class="{[classPrefix+'-item']: classPrefix, selected: value==='-'}" @click="selectType('-')">支出</li>
+      <li :class="{[classPrefix+'-item']: classPrefix, selected: value==='+'}">收入</li>
     </ul>
   </div>
 </template>
@@ -13,7 +13,8 @@ import {Component, Prop} from 'vue-property-decorator';
 
 @Component
 export default class Types extends Vue {
-  @Prop() readonly value!: string;
+  @Prop(String) readonly value!: string;
+  @Prop(String) classPrefix?: string;
   @Prop(Number) propA: number | undefined;
   //Prop 告诉 Vue propA 不是data 是 prop
   //Number 告诉 Vue xxx 运行时是个Number
