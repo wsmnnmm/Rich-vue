9c25993d349853288c44c270e5c33cc1b337909e
wsmnnmm <1832004940@qq.com>
2022-02-14 18:57:53 +0800

    项目部署

--- vue.config.js
+++ vue.config.js
@@ -2,19 +2,22 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Rich-vue/'
+    : '/',
   lintOnSave: false,
-  chainWebpack:config =>{
+  chainWebpack: config => {
     const dir = path.resolve('src/assets/icons')
 
     config.module
       .rule('svg-sprite')
       .test(/\.svg$/)
       .include.add(dir).end() //包含icons目录
-      .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract:false}).end()
+      .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract: false}).end()
       .use('svgo-loader').loader('svgo-loader')
       .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'),[{plainSprite:true}])
+    config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) //其他svg loader 排除icons目录
   }
 }
